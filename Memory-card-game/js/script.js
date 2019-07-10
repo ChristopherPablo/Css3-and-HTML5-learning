b791b529f576f8c99341db0ef1672941f2239f51
Cristofher Pablo <christopherguer.soni@hotmail.com>
2019-07-10 14:53:33 -0400

    Adding and building the cards

--- Memory-card-game/js/script.js
+++ Memory-card-game/js/script.js
@@ -1,13 +1,29 @@
 (function(){
+	var imagens = [];
+
+	for (var i = 0; i < 16; i++) {
+		var img = {
+			src: "img/"+ i + ".jpg",
+			id: i % 8
+		};
+
+		imagens.push(img);
+	}
+
 	startGame();
 
 	function startGame(){
+		var frontFaces = document.getElementsByClassName("front");
+
 		for (var i = 0; i < 16; i++) {
 			var card = document.querySelector("#card"+ i);
 			card.style.left = i % 8 === 0 ? 5 + "px" : i % 8 * 165 + 5 + "px";
 			card.style.top = i < 8 ? 5 + "px" : 250 + "px";
 
 			card.addEventListener("click", flipCard, false);
+
+			frontFaces[i].style.background = "url('" + imagens[i].src + "')";
+			frontFaces[i].setAttribute("id", imagens[i].id);
 		}
 	};
 
@@ -16,4 +32,6 @@
 		faces[0].classList.toggle("flipped");
 		faces[1].classList.toggle("flipped");
 	}
+
+
 }());
